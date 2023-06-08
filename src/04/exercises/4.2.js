3480cd6df3a982ccf35beef030cbf66277aed801
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-06-08 14:05:07 -0300

    exercise 4.2

--- src/04/exercises/4.2.js
+++ src/04/exercises/4.2.js
@@ -1,55 +1,37 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-//TODO entry point that checks if first dir is a valid dir. if so, calls process with files.length
+let processing = 0;
+const totalFiles = [];
 function listNestedFiles(dir, cb) {
-    let total = 0;
-    let processed = 0;
-    const totalFiles = [];
+    processing++;
 
-    fs.readdir(dir, (err, files) => {
+    fs.readdir(dir, { withFileTypes: true }, (err, files) => {
         if (err) {
-            return cb(err)
+            return cb(err);
         }
-        total = files.length;
-        files.forEach((file) => {
-        
-            if (isDir(path.resolve(dir, file))) {
-                listNestedFiles(path.resolve(dir, file), (err, files) => {
-                    if (err) {
-                        return cb(err)
-                    }
-                    // se supone que aca van a llegar solo los files? o pueden ser algunos dirs?
-                    files.forEach((file) => {
-                        totalFiles.push(file)
-                    })
-                    if(++processed === total) {
-                        return cb(null, totalFiles)
-                    }
-                })
+
+        files.forEach(file => {
+            if (file.isDirectory()) {
+                listNestedFiles(path.resolve(dir, file.name), cb);
             } else {
-                totalFiles.push(file)
-                if(++processed === total) {
-                    return cb(null, files)
-                }
+                totalFiles.push(file.name);
             }
-        })
-    })
+        });
 
+        if (--processing === 0) {
+            return cb(null, totalFiles);
+        }
+    });
 }
 
-function isDir(path) {
-    return fs.lstatSync(path).isDirectory() 
-}
-
-const dir = path.resolve(__dirname, 'test')
+const dir = path.resolve(__dirname, "test");
 listNestedFiles(dir, (err, files) => {
     if (err) {
-       return console.error(err);
+        return console.error(err);
     }
-    console.log(`NestedFiles: ${files}`)
-})
+    console.log(`NestedFiles: ${files}`);
+});
