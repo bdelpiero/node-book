2ed2a38937ab4b492bd6a9139c9956a7fdec286b
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-06-08 16:52:13 -0300

    add test case for 4.2

--- src/04/exercises/test/4.2.test.js
+++ src/04/exercises/test/4.2.test.js
@@ -1,5 +1,5 @@
 import path from "path";
-import {jest} from '@jest/globals'
+import { jest } from "@jest/globals";
 
 import { fileURLToPath } from "url";
 import { listNestedFiles } from "../4.2";
@@ -7,58 +7,66 @@ import { listNestedFiles } from "../4.2";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-beforeEach(() => jest.resetModules());
-test('should handle an empty dir', done => {
-    function callback(error, files) {
-      if (error) {
-        done(error);
-        return;
-      }
-      try {
-        expect(files).toHaveLength(0)
+test("should throw an error if invalid dir path", done => {
+    function callback(error) {
+        expect(error).toBeTruthy();
         done();
-      } catch (error) {
-        done(error);
-      }
     }
-    
-    const dir = path.resolve(__dirname, "case1");
-   listNestedFiles(dir, callback);
-});
-  
-test('should handle dir with no nested dirs', done => {
-    function callback(error, files) {
-      if (error) {
-        done(error);
-        return;
-      }
-      try {
-        expect(files).toHaveLength(3)
-        done();
-      } catch (error) {
-        done(error);
-      }
-    }
-    
-    const dir = path.resolve(__dirname, "case2");
-   listNestedFiles(dir, callback);
+
+    const dir = path.resolve(__dirname, "notADir");
+    listNestedFiles(dir, callback);
 });
 
-test('should handle nested dirs', done => {
+test("should handle an empty dir", done => {
     function callback(error, files) {
-      if (error) {
-        done(error);
-        return;
-      }
-      try {
-        console.log(files)
-        expect(files).toHaveLength(8)
-        done();
-      } catch (error) {
-        done(error);
-      }
+        if (error) {
+            done(error);
+            return;
+        }
+        try {
+            expect(files).toHaveLength(0);
+            done();
+        } catch (error) {
+            done(error);
+        }
     }
-    
+
+    const dir = path.resolve(__dirname, "case1");
+    listNestedFiles(dir, callback);
+});
+
+test("should handle dir with no nested dirs", done => {
+    function callback(error, files) {
+        if (error) {
+            done(error);
+            return;
+        }
+        try {
+            expect(files).toHaveLength(3);
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
+    const dir = path.resolve(__dirname, "case2");
+    listNestedFiles(dir, callback);
+});
+
+test("should handle nested dirs", done => {
+    function callback(error, files) {
+        if (error) {
+            done(error);
+            return;
+        }
+        try {
+            expect(files).toHaveLength(8);
+            done();
+        } catch (error) {
+            done(error);
+        }
+    }
+
     const dir = path.resolve(__dirname, "case3");
-   listNestedFiles(dir, callback);
+    listNestedFiles(dir, callback);
 });
