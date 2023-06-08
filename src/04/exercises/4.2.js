e2156b111208d1dca7ada8e1c2beb445861ea735
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-06-08 11:55:12 -0300

    wip solution exercise 4.2

--- src/04/exercises/4.2.js
+++ src/04/exercises/4.2.js
@@ -7,41 +7,43 @@ const __dirname = path.dirname(__filename);
 
 //TODO entry point that checks if first dir is a valid dir. if so, calls process with files.length
 function listNestedFiles(dir, cb) {
-    const files = [];
-    let processed = 0;
     let total = 0;
+    let processed = 0;
+    const totalFiles = [];
 
-    function process(currPath, entryPoint) {
-        fs.stat(currPath, (error, stats) => {
-            if (error) {
-                return cb(error);
-            }
-    
-            if (!stats.isDirectory() && entryPoint) {
-                return cb(new Error(`Not a dir`))
-            }
-            
-            if (stats.isFile()) {
-                files.push(currPath)
-            }
-            
-            if (++processed == total) {
-                return files;
-            }
-    
-            fs.readdir(currPath, (err, files) => {
-                if (err) {
-                    return cb(err)
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            return cb(err)
+        }
+        total = files.length;
+        files.forEach((file) => {
+        
+            if (isDir(path.resolve(dir, file))) {
+                listNestedFiles(path.resolve(dir, file), (err, files) => {
+                    if (err) {
+                        return cb(err)
+                    }
+                    // se supone que aca van a llegar solo los files? o pueden ser algunos dirs?
+                    files.forEach((file) => {
+                        totalFiles.push(file)
+                    })
+                    if(++processed === total) {
+                        return cb(null, totalFiles)
+                    }
+                })
+            } else {
+                totalFiles.push(file)
+                if(++processed === total) {
+                    return cb(null, files)
                 }
-                total += files.length;
-                files.forEach((file) => process(path.resolve(currPath, file), false ))
-            })
+            }
         })
+    })
 
-    }
+}
 
-    process(dir, true)
-
+function isDir(path) {
+    return fs.lstatSync(path).isDirectory() 
 }
 
 const dir = path.resolve(__dirname, 'test')
