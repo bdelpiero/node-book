50ee67d94ea3fe2498261b83b5b5824a9a007798
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-07-03 10:22:04 -0300

    exercise 5.2

---
+++ src/05/5.2.js
@@ -0,0 +1,40 @@
+export class TaskQueue {
+  constructor(concurrency) {
+      this.concurrency = concurrency;
+      this.running = 0;
+      this.queue = [];
+  }
+
+  // the resolve/reject handlers of the Promise are being added to the queue, 
+  // not the task itself. This way, if a task throws an error,
+  // the Promise that was returned originally by runTask will be rejected.
+  runTask(task) {
+      return new Promise((resolve, reject) => {
+          const taskWrapper = async () => {
+              try {
+                  const result = await task();
+                  return resolve(result);
+              } catch (error) {
+                  console.log('error')
+                  reject(error);
+              }
+          };
+
+          this.queue.push(taskWrapper);
+          process.nextTick(this.next.bind(this));
+      });
+  }
+
+  async next() {
+      while (this.running < this.concurrency && this.queue.length) {
+          const task = this.queue.shift();
+          this.running++;
+          try {
+              await task();
+          } finally {
+              this.running--;
+              this.next();
+          }
+      }
+  }
+}
---
+++ src/05/test/5.2.test.js
@@ -0,0 +1,41 @@
+import { jest } from "@jest/globals";
+import { TaskQueue } from "../5.2";
+
+describe("TaskQueue", () => {
+    let queue;
+
+    beforeEach(() => {
+        queue = new TaskQueue(2);
+    });
+
+    test("should run tasks", async () => {
+        const task1 = jest.fn().mockResolvedValue("Task 1");
+        const task2 = jest.fn().mockResolvedValue("Task 2");
+
+        const result1 = queue.runTask(task1);
+        const result2 = queue.runTask(task2);
+
+        await expect(result1).resolves.toEqual("Task 1");
+        await expect(result2).resolves.toEqual("Task 2");
+    });
+
+    test("should respect concurrency limit", async () => {
+        const task1 = jest.fn().mockResolvedValue("Task 1");
+        const task2 = jest.fn().mockResolvedValue("Task 2");
+        const task3 = jest.fn().mockResolvedValue("Task 3");
+
+        queue.runTask(task1);
+        queue.runTask(task2);
+        queue.runTask(task3);
+
+        expect(queue.running).toBeLessThanOrEqual(queue.concurrency);
+    });
+
+    test("should reject when task throws an error", async () => {
+        const failingTask = jest.fn().mockImplementation(() => {
+            throw new Error("Task error");
+        });
+
+        await expect(queue.runTask(failingTask)).rejects.toThrow("Task error");
+    });
+});
