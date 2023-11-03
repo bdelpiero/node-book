334c8f03bd451f56c5d00905e3bb7b3a398921e7
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-11-03 16:03:26 -0300

    Exercise 8.4

---
+++ src/08/8.4/fs-adapter.js
@@ -0,0 +1,46 @@
+import { resolve } from "path";
+
+export function createFSAdapter() {
+    const map = new Map();
+
+    return {
+        readFile(filename, options = {}, callback) {
+            if (typeof options === "function") {
+                // if options is a function, assume it is the callback (no options passed)
+                callback = options;
+                options = {};
+            } else if (typeof options === "string") {
+                options = { encoding: options };
+            }
+
+            const content = map.get(resolve(filename));
+            if (!content) {
+                const err = new Error(`ENOENT, open "${filename}"`);
+                err.code = "ENOENT";
+                err.errno = 34;
+                err.path = filename;
+                return callback && callback(err);
+            }
+
+            callback(null, content.toString(options.encoding));
+        },
+
+        // TODO should write to the in memory object
+        writeFile(filename, contents, options = {}, callback) {
+            if (typeof options === "function") {
+                // if options is a function, assume it is the callback (no options passed)
+                callback = options;
+                options = {};
+            } else if (typeof options === "string") {
+                options = { encoding: options };
+            }
+
+            try {
+                map.set(resolve(filename), Buffer.from(contents, options.encoding));
+                callback();
+            } catch (e) {
+                callback(e);
+            }
+        },
+    };
+}
