d163c4876914019b07e1490454888e4fe35b9bcb
Bruno Del Piero <bruno.delpiero@bluealba.com>
2023-12-20 17:06:50 -0300

    Exercise 10.1

---
+++ src/10/10.1/addPreInitQueue.js
@@ -0,0 +1,37 @@
+import { EventEmitter } from "events";
+
+export function addPreInitQueue(target, methods, eventName) {
+  if (!(target instanceof EventEmitter)) {
+    throw new TypeError("target obj must be an instance of EventEmitter");
+  }
+
+  let initialized = false;
+  let commandsQueue = [];
+
+  target.once(eventName, () => {
+    console.log(`Initialization event ${eventName} received. Running queued commands...`);
+    initialized = true;
+    commandsQueue.forEach(command => command());
+    commandsQueue = [];
+  });
+
+  const handler = {
+    get: (target, property) => {
+      if (!methods.includes(property) || initialized) {
+        return target[property];
+      }
+
+      return (...args) => {
+        console.log(`Call to ${property} queued with args ${JSON.stringify(args)}`);
+        return new Promise((resolve, reject) => {
+          const command = () => {
+            target[property](...args).then(resolve, reject);
+          };
+          commandsQueue.push(command);
+        });
+      };
+    },
+  };
+
+  return new Proxy(target, handler);
+}
